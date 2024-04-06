96d73f42d2fdfd69158d6dcabfa624498013ce76
davidmugambi104 <davidmugambi104@gmail.com>
2024-04-06 15:01:47 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraph = document.querySelector('#text');
+    paragraph.textContent = 'This is really cool!';
+});
+                       
